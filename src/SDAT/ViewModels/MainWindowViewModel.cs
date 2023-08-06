523d74c958b8aac96596cc34d886cc19603d3324
overdrive1708 <52090022+overdrive1708@users.noreply.github.com>
2023-08-06 10:57:02 +0900

    Add localization mechanism.

--- src/SDAT/ViewModels/MainWindowViewModel.cs
+++ src/SDAT/ViewModels/MainWindowViewModel.cs
@@ -4,7 +4,7 @@ namespace SDAT.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        private string _title = "Prism Application";
+        private string _title = Core.Resources.Strings.ApplicationName;
         public string Title
         {
             get { return _title; }
