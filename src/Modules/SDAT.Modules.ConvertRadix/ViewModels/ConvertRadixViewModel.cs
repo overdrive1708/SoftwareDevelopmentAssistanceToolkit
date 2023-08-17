d6b651966fc8901bb3792d94ef07aa68a764c2ee
overdrive1708 <52090022+overdrive1708@users.noreply.github.com>
2023-08-17 20:15:25 +0900

    Add ConvertRadis screen.

---
+++ src/Modules/SDAT.Modules.ConvertRadix/ViewModels/ConvertRadixViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Prism.Regions;
+using SDAT.Core.Mvvm;
+
+namespace SDAT.Modules.ConvertRadix.ViewModels
+{
+    public class ConvertRadixViewModel : RegionViewModelBase
+    {
+        //--------------------------------------------------
+        // メソッド
+        //--------------------------------------------------
+        /// <summary>
+        /// コンストラクタ(XAMLデザイナー用)
+        /// </summary>
+        public ConvertRadixViewModel()
+        {
+            // 無処理
+        }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="regionManager">IRegionManager</param>
+        public ConvertRadixViewModel(IRegionManager regionManager) :
+            base(regionManager)
+        {
+            // 無処理
+        }
+
+        /// <summary>
+        /// 画面遷移時処理
+        /// </summary>
+        /// <param name="navigationContext">ナビゲーションに関する情報</param>
+        public override void OnNavigatedTo(NavigationContext navigationContext)
+        {
+            // 無処理
+        }
+    }
+}
